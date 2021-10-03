66652563ced8b727f2f8f2fccac74d2feffb2f06
Atsocs <atsocs.cc@gmail.com>
2021-10-03 18:48:12 -0300

    login page

---
+++ src/components/Landing.js
@@ -0,0 +1,45 @@
+import './Landing.css'
+import React from "react";
+import LoginCard from './LoginCard';
+
+class Landing extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { 'page': props.initialPage }
+    }
+
+    render() {
+        let inner = (
+            <div className="main" >
+                Error: 'initialPage' prop must be set either to 'login' or to 'signUp'
+            </div>
+        )
+        if (this.state.page === 'login') {
+            inner = (
+                <div className="main">
+                    <LoginCard />
+                </div>
+            )
+        } else if (this.state.page === 'signUp') {
+            inner = (
+                <div className="main">
+                    {/* <SignUpCard /> */}
+                    SignUpCard
+                </div>
+            )
+        }
+        return (
+            <div className="wrapper">
+                <div id="background">
+                    <div className="background-top"></div>
+                    <div className="background-bottom"></div>
+                </div>
+                <div className="content">
+                    {inner}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
---
+++ src/components/LoginCard.js
@@ -0,0 +1,35 @@
+import './LoginCard.css'
+import React from 'react';
+
+
+class LoginCard extends React.Component {
+    login(e) {
+        e.preventDefault();
+        console.log("logged in")
+    }
+
+    redirectToSignUp() {
+        console.log("redirected to sign up")
+    }
+
+    render() {
+        return (
+            <div className="Card">
+                <h1>Chat App</h1>
+                <form onSubmit={this.login}>
+                    <input type="text" id="userame" name="userame" placeholder="username/email"></input><br />
+                    <input type="password" id="password" name="password" placeholder="password"></input><br />
+                    <div id="login-button"><input type="submit" value="Login" /></div>
+                </form>
+                <span id="unregistered-user-text">
+                    Don't have an account?&nbsp;
+                    <button className="linkButton" onClick={this.redirectToSignUp}>Click here</button>
+                    &nbsp;to Sign Up!
+                </span>
+            </div>
+        )
+    }
+}
+
+export default LoginCard
+
