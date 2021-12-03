a3458ad1d464d8fe118c3d620bf3bb09870dce0a
Atsocs <atsocs.cc@gmail.com>
2021-12-03 15:32:03 -0300

    refactor Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,51 @@
-import "./Input.css";
-import React from "react";
-
-class Input extends React.Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="icon" id="icon1"></div>
-        <div className="bar-wrapper">
-          <input className="bar" type="text"></input>
-        </div>
-        <div className="icon" id="icon2"></div>
+export default function Input() {
+  return (
+    <div style={styles.wrapper}>
+      {/* TODO Search / Back Icon */}
+      <div style={styles.icon({ radius: "4vh", top: "2vh", left: "2vw" })} />
+      <div style={styles.barWrapper}>
+        <input style={styles.bar} type="text" />
       </div>
-    );
-  }
+      {/* TODO Loading Icon */}
+      <div style={styles.icon({ radius: "3vh", top: "2.5vh", left: "27vw" })} />
+    </div>
+  );
 }
 
-export default Input;
+const styles = {
+  wrapper: {
+    height: "8vh",
+    backgroundColor: "var(--primaryColorVariant)",
+    position: "relative",
+  },
+  barWrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyItems: "center",
+    position: "relative",
+    height: "100%",
+  },
+  bar: {
+    margin: "0 1vw",
+    width: "100%",
+    height: "75%",
+    borderRadius: "4vh",
+    backgroundColor: "var(--primaryColorLight)",
+    border: "none",
+    paddingLeft: "4.8vw",
+    paddingRight: "2.5vw",
+    paddingTop: "0.5vh",
+    fontSize: "14px",
+    color: "var(--textColorLightVariant)",
+  },
+  icon: ({ radius, top, left }) => ({
+    position: "absolute",
+    width: radius,
+    height: radius,
+    borderRadius: "50%",
+    zIndex: 100,
+    backgroundColor: "var(--primaryColor)",
+    top: top,
+    left: left,
+  }),
+};
