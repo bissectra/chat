2156cfb2afabe3474a387a59ed22870bb03c58c1
Atsocs <atsocs.cc@gmail.com>
2021-12-03 18:54:26 -0300

    refactor UserIcon

--- src/components/UserIcon/index.js
+++ src/components/UserIcon/index.js
@@ -1,31 +1,13 @@
-import "./styles.css";
-
-import React from "react";
-
-class UserIcon extends React.Component {
-  constructor(props) {
-    super(props);
-    const sex = props.sex;
-    const sprites = sex === "M" ? "male" : sex === "F" ? "female" : "human";
-    this.state = {
-      sprites: sprites,
-    };
-  }
-
-  getSeed() {
-    const seed = "xxxxxxxx" + this.props.username;
-    return seed;
-  }
-
-  getUrl() {
-    const seed = this.getSeed();
-    const sprites = this.state.sprites;
-    return `url(https://avatars.dicebear.com/api/${sprites}/${seed}.svg?background=%23ffffff)`;
-  }
-
-  render() {
-    return <div id="icon-" style={{ backgroundImage: this.getUrl() }}></div>;
-  }
+export default function UserIcon({ username }) {
+  const seed = username;
+  const sprites = "jdenticon";
+  const url = `url(https://avatars.dicebear.com/api/${sprites}/${seed}.svg?background=%23ffffff)`;
+  return <div style={{ ...style, backgroundImage: url }} />;
 }
 
-export default UserIcon;
+const style = {
+  width: "6.5vh",
+  height: "6.5vh",
+  borderRadius: "50%",
+  backgroundColor: "white",
+};
