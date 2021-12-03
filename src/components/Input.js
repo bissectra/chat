037cb6bc09ab3f6de65189d7c98d12a5a2e3d5a1
Atsocs <atsocs.cc@gmail.com>
2021-12-03 18:30:59 -0300

    fix css bug in landing page

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ import React from "react";
 class Input extends React.Component {
     render() {
         return (
-            <div className="wrapper">
+            <div className="input-wrapper">
                 <div className="icon" id="icon1"></div>
                 <div className="bar-wrapper">
                     <input className="bar" type="text"></input>
