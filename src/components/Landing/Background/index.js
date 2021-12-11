6b6c484e2eade557b77615f8975e203e0b9b08a5
Atsocs <atsocs.cc@gmail.com>
2021-12-11 03:53:54 -0300

    remove w h

--- src/components/Landing/Background/index.js
+++ src/components/Landing/Background/index.js
@@ -1,6 +1,5 @@
 import "./styles.css";
 export default function Background({ children: inner }) {
-  const [w, h] = [window.innerWidth, window.innerHeight];
   return (
     <div className="wrapper">
       <div
