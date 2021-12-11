28f1c5ac0e191eea6d337276c2fc1b882c74afd2
Atsocs <atsocs.cc@gmail.com>
2021-12-11 03:51:00 -0300

    mudança de cores (houve uma tentativa de arrumar a backgroundimage do login)

--- src/components/Landing/Background/index.js
+++ src/components/Landing/Background/index.js
@@ -1,8 +1,12 @@
 import "./styles.css";
 export default function Background({ children: inner }) {
+  const [w, h] = [window.innerWidth, window.innerHeight];
   return (
     <div className="wrapper">
-      <div id="background">
+      <div
+        id="background"
+        style={{ backgroundImage: "url(https://i.imgur.com/jSgvC2l.jpg)" }}
+      >
         <div className="background-top"></div>
         <div className="background-bottom"></div>
       </div>
